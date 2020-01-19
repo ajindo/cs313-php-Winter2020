02697554c0aa2af9c8c2163981955ccb4ff3a61e
ajindo <ajindo@Atsushis-MacBook-Pro.local>
2020-01-18 20:25:56 -0700

    change in css

--- web/page_header.php
+++ web/page_header.php
@@ -2,18 +2,12 @@
 <div class = "navbar">
     <a
         href="home.php"
-        
     >
     Home
     </a>
     
     <a
         href="assignment.php"
-        <?php
-            if(basename($_SERVER['PHP_SELF']) == "assignment.php"){
-                echo "class=\"active\"";
-            }
-        ?>
     >
     Assignment
     </a>
