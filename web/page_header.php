06926645996508e5f1fc595af7e7f20613d23251
ajindo <ajindo@Atsushis-MacBook-Pro.local>
2020-01-18 19:08:29 -0700

    change in css

--- web/page_header.php
+++ web/page_header.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" type="text/css" href="homepage.css">
-<div id="header1">
-    <div  class = "navbar"
+<div id="header1" class = "navbar">
+    <div
         href="home.php"
         <?php
             if(basename($_SERVER['PHP_SELF']) == "home.php"){
@@ -11,7 +11,7 @@
     <a>Home</a>
     </div>
     
-    <div  class ="navbar"
+    <div
         href="assignment.php"
         <?php
             if(basename($_SERVER['PHP_SELF']) == "assignment.php"){
