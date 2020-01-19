c72d1a17ea2239a5bf04b5659eba7391041ed472
ajindo <ajindo@Atsushis-MacBook-Pro.local>
2020-01-18 18:34:44 -0700

    few change

--- web/page_header.php
+++ web/page_header.php
@@ -1,19 +1,21 @@
 <link rel="stylesheet" type="text/css" href="homepage.css">
 <h1>Atsushi's Home Page</h1>
 <div id="header1">
-    <a  herf="home.php"
+    <a  
         <?php
             if(basename($_SERVER['PHP_SELF']) == "home.php"){
                 echo "class=\"active\"";
             }
         ?>
+        herf="home.php"
     >Home</a>
     
-    <a  herf="assignmen.php"
+    <a  
         <?php
             if(basename($_SERVER['PHP_SELF']) == "assignment.php"){
                 echo "class=\"active\"";
             }
         ?>
+        herf="assignmen.php"
     >Assignment</a>
 </div>
