dd813b997aa08ba7d18f60c1876804b382bac764
ajindo <ajindo@Atsushis-MacBook-Pro.local>
2020-01-21 16:24:15 -0700

    team activity week 3 were added

---
+++ web/Team Activity/T3TA.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$continents = htmlspecialchars($_POST["continent[]"]);
+
+
+echo "Your name is $name" . "<br>" . "Your email is $email" . "<br>";
+
+echo "Your email is" <a href="mailto: $email."></a>
+
+
+$major = htmlspecialchars($_POST["major"]);
+$comment = htmlspecialchars($_POST["text"])
+
+echo "Your major is $major" . "<br>" . "Comments $comment" . "br>";
+
+?>
