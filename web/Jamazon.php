9232edc5103d21feb51b87d38e3f2f62fc191883
ajindo <ajindo@Atsushis-MacBook-Pro.local>
2020-01-22 20:30:00 -0700

    chaneg in inde.php and creating new web page for week3 prove

---
+++ web/Jamazon.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+
+</head>
+    <body>
+        <ul>
+            <li>
+               <a>
+                <image=    Black Thunder
+                </a>
+            </li>
+            <li>
+                Mitsuya Cyder
+            </li>
+            <li>
+                Soy Souce
+            </li>
+            <li>
+                Tooth Brush
+            </li>
+            <li>
+                Hair Wax
+            </li>
+            <li>
+                Mayo
+            </li>
+            <li>
+                Ketchup
+            </li>
+            <li>
+                Pepper & Salt
+            </li>
+            <li>
+                Deodorant
+            </li>
+            <li>
+                Nail Clipper
+            </li>
+        </ul>
+
+    </body>
+</html>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,41 @@
 <?php
 
-  phpinfo();
+  //phpinfo();
 
 ?>
+<!doctype html>
+<html>
+<head>
+  <title>Home Page<title>
+
+</head>
+<body>
+        
+        <div id="home1">
+          <div id="home2"> 
+            <?php
+                require('page_header.php')
+            ?>
+            <div id="home3">
+                <div>
+                  <img src="visa picture.jpg">
+                </div>
+                <div id="home4">
+                  <h2>Biography</h2>
+                  <p>Atsushi Jindo</p>
+                  <p>Major:     Computer Science</p>
+                  <p>Graduate:  2020</p>
+                  <p>I study at Brigham Young University - Idaho.
+                     My hobby is playing sports(Basketball, Valleyball, and Tennis),
+                      watching movies, and playing PC game(League of Legends).
+                  </p>
+                </div> 
+                <div id="home5"> 
+                  <p>Learning Programming Language:C++, C, Python, Java, HTML, PHP, CSS, Javascript, Swift</p>
+                  <p>Most interesting programming language is Python now. I want to create a useful AI in the future!</p>
+                </div>
+            </div>
+          </div>
+        </div>
+  </body>
+</html>
