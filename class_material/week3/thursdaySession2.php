ee8891b5c3421a70e37f239009b651d4c7336ace
ajindo <ajindo@Atsushis-MacBook-Pro.local>
2020-01-23 16:05:37 -0700

    adding new materials

--- class_material/week3/thursdaySession2.php
+++ class_material/week3/thursdaySession2.php
@@ -1,6 +1,10 @@
 <?php 
    // start session
+   session_start();
    // save session variables into local variables
+   $c = $_SESSION["favcolor"];
+   $a = $_SESSION["favanimal"];
+
 ?>
-<h1><?php // use the session variables ?></h1>
+<h1>Your favorite color is<?php $c?> and your favorite animal is <?php $a?></h1>
 <?php ?>
