e63fe5aacfdef7f9a880f48b463e93c29f9755aa
ajindo <ajindo@Atsushis-MacBook-Pro.local>
2020-01-22 18:06:51 -0700

    changing in team activity

--- web/Team Activity/T3TA.php
+++ web/Team Activity/T3TA.php
@@ -1,17 +1,10 @@
 <?php
-// First let's process all the input
-
-// using constants for the names of the elements in the form would be better...
-
-// It would also be better to use an ID of some sort for the
-// value that is submitted such as "cs" as opposed to "Computer Science",
-// then in PHP we could process that value and determine the exact
-// presentation text to render.
 $name = htmlspecialchars($_POST["name"]);
 $email = htmlspecialchars($_POST["email"]);
-$major = htmlspecialchars($_POST["major"]);
 $places = $_POST["continents"];
-$comments = htmlspecialchars($_POST["comments"]);
+$major = htmlspecialchars($_POST["major"]);
+$comment = htmlspecialchars($_POST["comments"]);
+
 
 ?>
 <!DOCTYPE html>
@@ -22,7 +15,6 @@ $comments = htmlspecialchars($_POST["comments"]);
 
 <body>
 	<h1>Submission Results</h1>
-
 	<p>Your name is: <?=$name ?></p>
 	<p>Your email is: <a href="mailto:<?=$email ?>"><?=$email ?></a></p>
 	<p>Your major is: <?=$major ?></p>
@@ -39,7 +31,7 @@ foreach ($continents as $place)
 
 	</ul>
 
-	<p>Comments: <?=$comments?></p>
+	<p>Comments: <?=$comments ?></p>
 
 </body>
 
